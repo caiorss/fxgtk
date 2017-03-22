49c6f77192966915771e6d074233528fb095a54f
CaioRS <caiorss.rodrigues@gmail.com>
2017-03-21 23:12:34 -0300

    Add load script.

---
+++ loader.fsx
@@ -0,0 +1,16 @@
+//  Minimal script to load fxgtk and its dependencies.
+//
+//
+//
+
+
+#if INTERACTIVE    
+#r "/usr/lib/mono/gtk-sharp-3.0/atk-sharp.dll"   
+#r "/usr/lib/mono/gtk-sharp-3.0/gio-sharp.dll"
+#r "/usr/lib/mono/gtk-sharp-3.0/glib-sharp.dll"
+#r "/usr/lib/mono/gtk-sharp-3.0/gtk-sharp.dll"
+#r "/usr/lib/mono/gtk-sharp-3.0/gdk-sharp.dll"
+#endif
+
+#r "fxgtk.dll"
+  
