ea5e8f6bc6ef09bfb1885bbfb68c7754b39f475a
CaioRS <caiorss.rodrigues@gmail.com>
2017-06-02 16:40:54 -0300

    Add file build-script.fsx.

---
+++ build-script.fsx
@@ -0,0 +1,106 @@
+// #!/usr/bin/env fsharpi
+
+open System
+
+let gtkHome = "/usr/lib/mono/gtk-sharp-3.0/"
+
+let gtkDependencies =  
+    let gtkDlls = [
+        "atk-sharp.dll"
+        ;"gio-sharp.dll"
+        ;"glib-sharp.dll"
+        ;"gtk-sharp.dll"
+        ;"gdk-sharp.dll"
+        ;"cairo-sharp.dll"
+        ;"pango-sharp.dll"
+        ]
+    List.map (fun p -> System.IO.Path.Combine(gtkHome, p)) gtkDlls
+
+module SysUtils = 
+
+    let runShellCmd (program: String) (args: string list) =
+        let argString = String.Join(" ", args)
+        ignore <| System.Diagnostics.Process.Start(program, argString)
+
+    let joinPath path1 path2 =
+        System.IO.Path.Combine(path1, path2)
+
+    let baseName file =
+        System.IO.Path.GetFileName file 
+
+    let replaceExt ext file =
+        System.IO.Path.ChangeExtension(file, ext)
+
+    let getFilesExt path pattern =
+        System.IO.Directory.GetFiles(path, pattern)
+        |> Seq.ofArray
+
+    let getCommandLineArgs () =
+        let args = Environment.GetCommandLineArgs()
+        let idx = Array.tryFindIndex (fun a -> a = "--") args
+        match idx with
+        | None   -> []
+        | Some i -> args.[(i+1)..] |> List.ofArray
+
+
+type FsharpCompiler =
+    static member CompileLibrary(sources:      string list 
+                                ,dependencies: string list 
+                                ,output
+                                ,doc
+                                 )  =
+
+        let args = [ String.Join(" ", sources)
+                   ; "--target:library"
+                   ; "--out:" + output
+                   ; "--doc:" + doc                  
+                   ; String.Join(" ", dependencies |> List.map (fun s -> "-r:" + s))
+                   ]
+
+        printfn "%A" args 
+        SysUtils.runShellCmd "fsharpc" args 
+
+
+    static member CompileExecutableWEXE( sources:      string list 
+                                        ,dependencies: string list 
+                                        ,output                             
+                                        )  =
+
+        let args = [ String.Join(" ", sources)
+                   ; "--target:winexe"
+                   ; "--out:" + output              
+                   ; String.Join(" ", dependencies |> List.map (fun s -> "-r:" + s))
+                   ]
+
+        // printfn "%A" args 
+        SysUtils.runShellCmd "fsharpc" args 
+
+
+
+
+let buildExample example =
+    FsharpCompiler.CompileExecutableWEXE(
+        [SysUtils.joinPath "examples/" example]
+        ,["bin/fxgtk.dll"] @ gtkDependencies
+        , example |> SysUtils.joinPath "bin/" |> SysUtils.replaceExt "exe" 
+        )
+
+
+let runArgs args =
+    match args with
+        
+    // Show all examples  
+    | ["--example"]
+      -> SysUtils.getFilesExt "examples" "*.fsx"
+      |> Seq.map SysUtils.baseName
+      |> Seq.iter (printfn "%s")
+    
+    // Build a given example 
+    | ["--example"; fileName] -> buildExample fileName
+    
+    | cmd -> printfn "Error: Invalid command: %A" cmd
+
+
+
+let () =
+    runArgs <| SysUtils.getCommandLineArgs() 
